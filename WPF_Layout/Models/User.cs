a5d4dcb26b0f778ca1236469e9507f015a422cff
TruongHoangTanDugn <student242305@ptnk.edu.vn>
2026-03-01 01:08:44 +0700

    Chore: Clean up repo and apply new .gitignore file

---
+++ WPF_Layout/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WPF_Layout.Models
+{
+    [Table("Users")] 
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
