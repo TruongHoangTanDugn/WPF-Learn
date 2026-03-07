133a6c4237f020ce603054acfb7c9743e59d6e69
Trương Hoàng Tấn Dũng <139193530+TruongHoangTanDugn@users.noreply.github.com>
2026-03-08 00:53:57 +0700

    Add files via upload
    Add folder Data

---
+++ WPF_Layout/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WPF_Layout.Models;
+
+namespace WPF_Layout.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public AppDbContext() 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=WPFAppLearnDB.db");
+            }
+        }
+    }
+}
